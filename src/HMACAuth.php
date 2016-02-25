3a522993f61e610b25d10dffcdfc0f30a960ddcd
Marcel Hernandez <e6990620@gmail.com>
2016-02-25 20:14:42 +0100

    Proof of concept

---
+++ src/HMACAuth.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace OneMA;
+
+use Psr\Http\Message\MessageInterface;
+
+class HMACAuth
+{
+    const AUTH_HEADER = 'Authorization';
+    const HMAC_HASH = 'sha256';
+
+    /**
+     * @param MessageInterface $message
+     * @param string           $secret
+     *
+     * @return MessageInterface The signed message
+     */
+    public static function sign(MessageInterface $message, $secret)
+    {
+        return $message->withHeader(
+            self::AUTH_HEADER,
+            self::calculateHMACSig(\GuzzleHttp\Psr7\str($message), $secret)
+        );
+    }
+
+    /**
+     * @param MessageInterface $message
+     * @param string           $secret
+     *
+     * @return bool Signature verification outcome
+     */
+    public static function verify(MessageInterface $message, $secret)
+    {
+        if (empty($clientSideSignature = $message->getHeaderLine(self::AUTH_HEADER))) {
+            return false;
+        }
+
+        $serverSideSignature = self::calculateHMACSig(
+            \GuzzleHttp\Psr7\str($message->withoutHeader(self::AUTH_HEADER)),
+            $secret
+        );
+
+        return hash_equals($serverSideSignature, $clientSideSignature);
+    }
+
+    private static function calculateHMACSig($payload, $secret)
+    {
+        return rtrim(strtr(base64_encode(hash_hmac(self::HMAC_HASH, $payload, $secret, true)), '+/', '-_'), '=');
+    }
+}
---
+++ test/demo.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+// Client code
+$request = new \GuzzleHttp\Psr7\Request('GET', 'http://example.com');
+$signedRequest = OneMA\HMACAuth::sign($request, '$ecr3t');
+
+// Signed request is sent to server...
+
+// Server code
+var_dump(OneMA\HMACAuth::verify($signedRequest, '$ecr3t'));
