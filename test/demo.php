1969d4cf9a1b59fb6ff0df888c67d481741213e9
Marcel Hernandez <e6990620@gmail.com>
2016-05-20 19:19:16 +0200

    quick test Zend Psr7 implementation

--- test/demo.php
+++ test/demo.php
@@ -10,3 +10,9 @@ $signedRequest = OneMA\HMACAuth::sign($request, '$ecr3t');
 
 // Server code
 var_dump(OneMA\HMACAuth::verify($signedRequest, '$ecr3t'));
+
+
+// Again, with Diactoros...
+$zendRequest = new \Zend\Diactoros\Request('http://example.com', 'GET');
+$zendSignedRequest = OneMA\HMACAuth::sign($request, '$ecr3t');
+var_dump(OneMA\HMACAuth::verify($zendSignedRequest, '$ecr3t'));
