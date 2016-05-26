8f71ee3071144b3925f554629894e070321d0077
Marcel Hernandez <e6990620@gmail.com>
2016-05-26 23:19:32 +0200

    phyrexia/http got itself disqualified since it appears to be just a thin layer of cruft around guzzlehttp/psr7.
    see  https://github.com/jejem/http-client/commit/a611f6e9b4eb7d0c154db2f481c854fe4545e723

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -4,7 +4,6 @@ namespace UMA\Tests\Psr\Http\Message;
 
 use UMA\Tests\Psr\Http\Message\Factory\AsikaFactory;
 use UMA\Tests\Psr\Http\Message\Factory\GuzzleFactory;
-use UMA\Tests\Psr\Http\Message\Factory\PhyrexiaFactory;
 use UMA\Tests\Psr\Http\Message\Factory\RingCentralFactory;
 use UMA\Tests\Psr\Http\Message\Factory\SlimFactory;
 use UMA\Tests\Psr\Http\Message\Factory\WanduFactory;
@@ -30,7 +29,6 @@ abstract class AbstractTestCase extends \PHPUnit_Framework_TestCase
         self::$requestProvider = (new RequestProvider())
             ->addFactory($asikaFactory = new AsikaFactory())
             ->addFactory($guzzleFactory = new GuzzleFactory())
-            ->addFactory($phyrexiaFactory = new PhyrexiaFactory())
             ->addFactory($ringCentralFactory = new RingCentralFactory())
             ->addFactory($slimFactory = new SlimFactory())
             ->addFactory($wanduFactory = new WanduFactory())
@@ -39,7 +37,6 @@ abstract class AbstractTestCase extends \PHPUnit_Framework_TestCase
         self::$responseProvider = (new ResponseProvider())
             ->addFactory($asikaFactory)
             ->addFactory($guzzleFactory)
-            ->addFactory($phyrexiaFactory)
             ->addFactory($ringCentralFactory)
             ->addFactory($slimFactory)
             ->addFactory($wanduFactory)
