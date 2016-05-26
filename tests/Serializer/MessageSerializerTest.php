c1a847fb6691c3fabb752fad145abfcb3cc8810d
Marcel Hernandez <e6990620@gmail.com>
2016-05-27 00:30:45 +0200

    remerge MessageSerializer tests

--- tests/Serializer/MessageSerializerTest.php
+++ tests/Serializer/MessageSerializerTest.php
@@ -7,6 +7,85 @@ use UMA\Tests\Psr\Http\Message\AbstractTestCase;
 
 class MessageSerializerTest extends AbstractTestCase
 {
+    /**
+     * @dataProvider requestsProvider
+     *
+     * @param string   $method
+     * @param string   $url
+     * @param string[] $headers
+     * @param string   $expectedSerialization
+     */
+    public function testRequests($method, $url, array $headers, $expectedSerialization)
+    {
+        foreach (self::$requestProvider->shotgun($method, $url, $headers, null) as $request) {
+            $actualSerialization = MessageSerializer::serialize($request);
+
+            $this->assertSame($expectedSerialization, $actualSerialization, get_class($request));
+        }
+    }
+
+    public function requestsProvider()
+    {
+        return [
+            'simple requests' => [
+                'GET',
+                'http://www.example.com/index.html',
+                [],
+                "GET /index.html HTTP/1.1\r\nHost: www.example.com\r\n\r\n",
+            ],
+
+            'headed requests' => [
+                'GET',
+                'http://www.example.com/index.html',
+                [
+                    'User-Agent' => 'PHP/5.6.21',
+                    'Accept' => '*/*',
+                    'Connection' => 'keep-alive',
+                    'Accept-Encoding' => 'gzip, deflate',
+                ],
+                "GET /index.html HTTP/1.1\r\nHost: www.example.com\r\nAccept: */*\r\nAccept-Encoding: gzip, deflate\r\nConnection: keep-alive\r\nUser-Agent: PHP/5.6.21\r\n\r\n",
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider responsesProvider
+     *
+     * @param int      $statusCode
+     * @param string[] $headers
+     * @param string   $expectedSerialization
+     */
+    public function testResponses($statusCode, array $headers, $expectedSerialization)
+    {
+        foreach (self::$responseProvider->shotgun($statusCode, $headers, null) as $request) {
+            $actualSerialization = MessageSerializer::serialize($request);
+
+            $this->assertSame($expectedSerialization, $actualSerialization, get_class($request));
+        }
+    }
+
+    public function responsesProvider()
+    {
+        return [
+            'simple responses' => [
+                200,
+                [],
+                "HTTP/1.1 200 OK\r\n\r\n",
+            ],
+
+            'headed responses' => [
+                200,
+                [
+                    'Content-Type' => 'text/html',
+                    'Content-Encoding' => 'gzip',
+                    'Accept-Ranges' => 'bytes',
+                    'Content-Length' => '606',
+                ],
+                "HTTP/1.1 200 OK\r\nAccept-Ranges: bytes\r\nContent-Encoding: gzip\r\nContent-Length: 606\r\nContent-Type: text/html\r\n\r\n",
+            ],
+        ];
+    }
+
     public function testSerializeNeitherRequestNorResponse()
     {
         $this->setExpectedException(\InvalidArgumentException::class);
