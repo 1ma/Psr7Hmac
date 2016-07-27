79779456f1d16c12d33019856565038a84b1bee5
Marcel Hernandez <e6990620@gmail.com>
2016-07-27 12:29:00 +0200

    update comment

--- resources/boris.php
+++ resources/boris.php
@@ -1,10 +1,10 @@
 #!/usr/bin/env php
 <?php
 
-// This debugging script is meant to be run from inside an 1maa/php-dev Docker container.
+// This debugging script is meant to be run from inside a development 1maa/php-cli Docker container.
 
 // Alternatively you can put a boris.phar in the expected path of your system,
-// or simply change it to point somewhere else.
+// or simply change the require_once to point somewhere else.
 
 require_once 'phar:///usr/local/bin/boris.phar/lib/autoload.php';
 require_once __DIR__.'/../vendor/autoload.php';
